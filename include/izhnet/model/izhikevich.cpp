8d63457b71f20d6f618f4d23e7df1b3619baa74b
juliansalvador727 <juliansalvador727@gmail.com>
2026-02-10 19:26:27 -0500

    dv/dt du/dt

--- include/izhnet/model/izhikevich.cpp
+++ include/izhnet/model/izhikevich.cpp
@@ -4,9 +4,18 @@
 
 namespace izhnet {
 
+static inline double dv_dt(double V, double U, double I) { return 0.04 * (V*V) + (5*V) + 140 - U + I; }
+static inline double du_dt(const IzhParams& p, double V, double U) {return p.a * (p.b * V - U); }
+
 bool step_izhikevich(double& V, double& U, double I, double dt_ms, const IzhParams& p)
 {
+    bool spiked{ false };
     // gotta set this up fr
+    if (p.consistent_integration) {
+        // dont use nest
+    } else {
+        // use nest
+    }
 
     (void)I;
     (void)dt_ms;
